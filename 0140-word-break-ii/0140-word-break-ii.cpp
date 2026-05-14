4a09dd3f77df936e149dfaf0bc95835607c15269
Meghana <meghanadandu33@gmail.com>
2026-05-14 18:52:20 +0530

    Time: 0 ms (100%), Space: 11.2 MB (7.67%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    unordered_set<string> dict;
+
+    unordered_map<string, vector<string>> memo;
+
+    vector<string> solve(string s) {
+
+        if (memo.count(s))
+            return memo[s];
+
+        vector<string> result;
+
+        if (s.empty()) {
+            result.push_back("");
+            return result;
+        }
+
+        for (int i = 1; i <= s.size(); i++) {
+
+            string word = s.substr(0, i);
+
+            if (dict.count(word)) {
+
+                string remaining = s.substr(i);
+
+                vector<string> subSentences = solve(remaining);
+
+                for (string sentence : subSentences) {
+
+                    if (sentence.empty())
+                        result.push_back(word);
+                    else
+                        result.push_back(word + " " + sentence);
+                }
+            }
+        }
+
+        return memo[s] = result;
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+
+        dict = unordered_set<string>(wordDict.begin(), wordDict.end());
+
+        return solve(s);
+    }
+};
