eb54a36e33053fb05f3ad635d947e0ee137c1ab1
Meghana <meghanadandu33@gmail.com>
2026-05-17 15:53:51 +0530

    Time: 1 ms (82.96%), Space: 12.4 MB (49.25%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,13 @@
+class Solution:
+    def finalPrices(self, prices):
+        stack = []
+
+        for i in range(len(prices)):
+
+            while stack and prices[stack[-1]] >= prices[i]:
+                index = stack.pop()
+                prices[index] -= prices[i]
+
+            stack.append(i)
+
+        return prices
