c9b7fac5d339655f987d94a85db7eb14fcaf29c7
Meghana <meghanadandu33@gmail.com>
2026-05-18 21:33:55 +0530

    Time: 153 ms (83.77%), Space: 26.8 MB (63.6%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []   # stores indices
+
+        for i in range(n):
+            # Check if current temperature is warmer
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+
+            stack.append(i)
+
+        return answer
